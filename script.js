d30901a7a9984a42c1b5156b8305ad2db15aeffe
aleya0 <91791106+aleya0@users.noreply.github.com>
2021-10-02 20:31:35 +0530

    remove consoles

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
   $(document).ready(function() {
         var url = "https://api.covid19india.org/data.json"
         $.getJSON(url, function(data) {
-            console.log(data);
             var total_active, total_recovered, total_deaths, total_confirmed
             total_active = data.statewise[0].active
             total_recovered = data.statewise[0].recovered
